274e57141f8a698c84d87eaa7b37309be0089e30
Fengguang Wu <fengguang.wu@intel.com>
2018-08-23 22:34:38 +0800

    page-refs: align coding style with linux kernel
    wfg /c/pmem2dram% /c/linux/scripts/Lindent  page-refs.c
    wfg /c/pmem2dram% /c/linux/scripts/checkpatch.pl --fix-inplace -f page-refs.c
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- page-refs.c
+++ page-refs.c
@@ -16,7 +16,7 @@
 #define IDLEMAP_BUF_SIZE	4096
 
 // big enough to span 640 Gbytes:
-#define MAX_IDLEMAP_SIZE	(20 * 1024 * 1024)		//20M * 8 * 4K = 640G ?
+#define MAX_IDLEMAP_SIZE	(20 * 1024 * 1024)	//20M * 8 * 4K = 640G ?
 #define MAX_FILE_PATH	255
 
 //#define DEBUG
@@ -29,15 +29,15 @@
 #endif
 
 // globals
-int g_debug = 0;		// 1 == some, 2 == verbose
-unsigned long long *g_idlebuf = NULL;
-unsigned long long g_idlebufsize = 0;
-unsigned short *g_refs_count = NULL;
-unsigned long long g_offset = 0, g_size = 0;
-unsigned long long g_start_pfn = 0, g_num_pfn = 0;
+int g_debug;			// 1 == some, 2 == verbose
+unsigned long long *g_idlebuf;
+unsigned long long g_idlebufsize;
+unsigned short *g_refs_count;
+unsigned long long g_offset, g_size;
+unsigned long long g_start_pfn, g_num_pfn;
 char g_setidle_buf[IDLEMAP_BUF_SIZE];
 int g_setidle_bufsize;
-int g_idlefd = 0;
+int g_idlefd;
 
 int output_refs(const char *output_file)
 {
@@ -53,8 +53,10 @@ int output_refs(const char *output_file)
 	}
 
 	fprintf(file, "%-18s  refcount\n", "PFN");
-	for (pfn = 0; pfn < g_num_pfn; pfn ++) {
-		fprintf(file, "0x%016llx  %d\n", g_start_pfn + pfn, g_refs_count[pfn]);
+	for (pfn = 0; pfn < g_num_pfn; pfn++) {
+		fprintf(file, "0x%016llx  %d\n",
+			g_start_pfn + pfn,
+			g_refs_count[pfn]);
 	}
 	fclose(file);
 	return 0;
@@ -66,15 +68,16 @@ int account_refs(void)
 	unsigned long long idlebits, idlemap = 0, base_pfn = 0, pfn;
 
 	while (len < g_idlebufsize) {
-		// need to check ? 
+		// need to check ?
 		idlebits = g_idlebuf[idlemap];
 		debug_printf("idlebits: 0x%llx\n", idlebits);
 
-		for (pfn = 0; pfn < 64; pfn ++) {
+		for (pfn = 0; pfn < 64; pfn++) {
 			if (!(idlebits & (1ULL << pfn))) {
-				g_refs_count[base_pfn + pfn] ++; 
+				g_refs_count[base_pfn + pfn]++;
 				debug_printf("pfn 0x%llx refs_count 0x%llx\n",
-			    		base_pfn + pfn, g_refs_count[base_pfn + pfn]);
+					     base_pfn + pfn,
+					     g_refs_count[base_pfn + pfn]);
 			}
 		}
 
@@ -117,9 +120,9 @@ int loadidlemap(unsigned long long offset, unsigned long long size)
 		goto _loadidlemap_out;
 	}
 
-	p = (char*)g_idlebuf;
+	p = (char *)g_idlebuf;
 	g_idlebufsize = 0;
-	
+
 	// unfortunately, larger reads do not seem supported
 	while ((len = read(g_idlefd, p, IDLEMAP_CHUNK_SIZE)) > 0) {
 		p += len;
@@ -148,15 +151,15 @@ static const struct option opts[] = {
 static void usage(char *prog)
 {
 	fprintf(stderr,
-			"%s [option] ...\n"
-			"    -h|--help		Show this information.\n"
-			"    -o|--offset	The start offset of the bitmap to be scanned.\n"
-			"    -s|--size		The size should be scanned.\n"
-			"    -i|--interval	The interval to scan bitmap.\n"
-			"    -l|--loop		The number of times to scan bitmap.\n"
-			"    -b|--bitmap	The bitmap file for scanning.\n"
-			"    -f|--output	The output file for the result of scanning.\n",
-			prog);
+		"%s [option] ...\n"
+		"    -h|--help		Show this information.\n"
+		"    -o|--offset	The start offset of the bitmap to be scanned.\n"
+		"    -s|--size		The size should be scanned.\n"
+		"    -i|--interval	The interval to scan bitmap.\n"
+		"    -l|--loop		The number of times to scan bitmap.\n"
+		"    -b|--bitmap	The bitmap file for scanning.\n"
+		"    -f|--output	The output file for the result of scanning.\n",
+		prog);
 }
 
 int main(int argc, char *argv[])
@@ -168,7 +171,7 @@ int main(int argc, char *argv[])
 	int pagesize, optind, opt = 0, options_index = 0;
 	char bitmap_file[MAX_FILE_PATH] = "bitmap_file";
 	char output_file[MAX_FILE_PATH] = "output_file";
-	const char *optstr="ho:s:i:l:b:f:";
+	const char *optstr = "ho:s:i:l:b:f:";
 	unsigned long long offset = 0, size = 0, bufsize;
 	unsigned long long set_us, read_us, dur_us, slp_us, account_us, est_us;
 	static struct timeval ts1, ts2, ts3, ts4, ts5;
@@ -177,58 +180,60 @@ int main(int argc, char *argv[])
 	pagesize = getpagesize();
 
 	/* parse the options */
-	while((opt = getopt_long(argc, argv, optstr, opts, &options_index)) != EOF) {
-		switch(opt) {
-			case 0:
-				break;
-			case 'o':
-				offset = strtoul(optarg, NULL, 16);
-				debug_printf("offset = 0x%llx\n", offset);
-				offset /= pagesize * 8;
-				debug_printf("offset of the bitmap = 0x%llx, pagesize = 0x%x\n", offset, pagesize);
-				break;
-			case 's':
-				size = strtoul(optarg, NULL, 16);
-				debug_printf("size = 0x%llx\n", size);
-				size /= pagesize * 8;
-				debug_printf("size of the bitmap = 0x%llx, pagesize = 0x%x\n", size, pagesize);
-				break;
-			case 'i':
-				interval = atof(optarg);
-				debug_printf("interval = %.2f\n", interval);
-				break;
-			case 'l':
-				loop = atoi(optarg);
-				debug_printf("loop number = %d\n", loop);
-				break;
-			case 'b':
-				strcpy(bitmap_file, optarg);
-				debug_printf("bitmap_file = %s\n", bitmap_file);
-				break;
-			case 'f':
-				strcpy(output_file, optarg);
-				debug_printf("output_file = %s\n", output_file);
-				break;
-			case 'h':
-				usage(argv[0]);
-				exit(0);
-			case '?':
-			default:
-				usage(argv[0]);
-				exit(0);
+	while ((opt = getopt_long(argc, argv, optstr, opts, &options_index)) != EOF) {
+		switch (opt) {
+		case 0:
+			break;
+		case 'o':
+			offset = strtoul(optarg, NULL, 16);
+			debug_printf("offset = 0x%llx\n", offset);
+			offset /= pagesize * 8;
+			debug_printf("offset of the bitmap = 0x%llx, pagesize = 0x%x\n", offset, pagesize);
+			break;
+		case 's':
+			size = strtoul(optarg, NULL, 16);
+			debug_printf("size = 0x%llx\n", size);
+			size /= pagesize * 8;
+			debug_printf("size of the bitmap = 0x%llx, pagesize = 0x%x\n", size, pagesize);
+			break;
+		case 'i':
+			interval = atof(optarg);
+			debug_printf("interval = %.2f\n", interval);
+			break;
+		case 'l':
+			loop = atoi(optarg);
+			debug_printf("loop number = %d\n", loop);
+			break;
+		case 'b':
+			strcpy(bitmap_file, optarg);
+			debug_printf("bitmap_file = %s\n", bitmap_file);
+			break;
+		case 'f':
+			strcpy(output_file, optarg);
+			debug_printf("output_file = %s\n", output_file);
+			break;
+		case 'h':
+			usage(argv[0]);
+			exit(0);
+		case '?':
+		default:
+			usage(argv[0]);
+			exit(0);
 		}
-	}	
+	}
 
-	printf("Watching the %s to calculate page reference count "
-		   "every %.2f seconds for %d times.\n"
-		   "The result will be saved in %s. \n",
-		   bitmap_file, interval, loop, output_file);
+	printf("Watching the %s to calculate page reference count every %.2f seconds for %d times.\n"
+	       "The result will be saved in %s.\n",
+	       bitmap_file,
+	       interval,
+	       loop,
+	       output_file);
 
 	if (!size)
 		size = MAX_IDLEMAP_SIZE;
 
-	g_setidle_bufsize = size < sizeof (g_setidle_buf) ?
-						size : sizeof (g_setidle_buf);
+	g_setidle_bufsize = size < sizeof(g_setidle_buf) ?
+			    size : sizeof(g_setidle_buf);
 	for (i = 0; i < g_setidle_bufsize; i++)
 		g_setidle_buf[i] = 0xff;
 
@@ -236,13 +241,13 @@ int main(int argc, char *argv[])
 		perror("Can't write bitmap file");
 		exit(2);
 	}
-
 	//need to align on IDLEMAP_CHUNK_SIZE ?
 	bufsize = size;
 	bufsize += IDLEMAP_CHUNK_SIZE - 1;
 	bufsize = bufsize / IDLEMAP_CHUNK_SIZE * IDLEMAP_CHUNK_SIZE;
- 	if ((g_idlebuf = malloc(bufsize)) == NULL) {
-		printf("Can't allocate memory for idlemap buf (%d bytes)!\n", bufsize);
+	if ((g_idlebuf = malloc(bufsize)) == NULL) {
+		printf("Can't allocate memory for idlemap buf (%d bytes)!\n",
+		       bufsize);
 		ret = -1;
 		goto out;
 	}
@@ -251,17 +256,17 @@ int main(int argc, char *argv[])
 	g_start_pfn = offset * 8;
 	g_num_pfn = size * 8;
 	debug_printf("The start pfn is 0x%llx, the number of pfn is 0x%llx.\n",
-		   		 g_start_pfn, g_num_pfn);
+		     g_start_pfn, g_num_pfn);
 
 	if ((g_refs_count = malloc(sizeof(unsigned short) * g_num_pfn)) == NULL) {
 		printf("Can't allocate memory for idlemap buf (%d bytes)!\n",
-				sizeof(unsigned short) * g_num_pfn);
+		       sizeof(unsigned short) * g_num_pfn);
 		ret = -2;
 		goto out;
 	}
 	memset(g_refs_count, 0, sizeof(unsigned short) * g_num_pfn);
 
-	for (i = 0; i < loop; i ++) {
+	for (i = 0; i < loop; i++) {
 		// set idle flags
 		gettimeofday(&ts1, NULL);
 		ret = setidlemap(offset, size);
@@ -276,27 +281,28 @@ int main(int argc, char *argv[])
 		// read idle flags
 		ret = loadidlemap(offset, size);
 		if (ret) {
-			goto out;	
+			goto out;
 		}
 		gettimeofday(&ts4, NULL);
 
 		ret = account_refs();
 		if (ret) {
-			goto out;	
+			goto out;
 		}
 		gettimeofday(&ts5, NULL);
 
+		//TODO: create a routine func to simplify the below calculations
 		// calculate times
 		set_us = 1000000 * (ts2.tv_sec - ts1.tv_sec) +
-		    (ts2.tv_usec - ts1.tv_usec);
+				   (ts2.tv_usec - ts1.tv_usec);
 		slp_us = 1000000 * (ts3.tv_sec - ts2.tv_sec) +
-		    (ts3.tv_usec - ts2.tv_usec);
+				   (ts3.tv_usec - ts2.tv_usec);
 		read_us = 1000000 * (ts4.tv_sec - ts3.tv_sec) +
-		    (ts4.tv_usec - ts3.tv_usec);
+				    (ts4.tv_usec - ts3.tv_usec);
 		account_us = 1000000 * (ts5.tv_sec - ts4.tv_sec) +
-		    (ts5.tv_usec - ts4.tv_usec);
+				       (ts5.tv_usec - ts4.tv_usec);
 		dur_us = 1000000 * (ts5.tv_sec - ts1.tv_sec) +
-	    	(ts5.tv_usec - ts1.tv_usec);
+				   (ts5.tv_usec - ts1.tv_usec);
 
 		if (g_debug) {
 			printf("set time   : %.6f s\n", (double)set_us / 1000000);
@@ -313,7 +319,7 @@ out:
 	if (g_idlefd)
 		close(g_idlefd);
 
-	if (g_idlebuf) 
+	if (g_idlebuf)
 		free(g_idlebuf);
 	if (g_refs_count)
 		free(g_refs_count);
