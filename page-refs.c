ae5e477b731c3c2a6f56d2bf2ed4a7b0659f89c4
Fengguang Wu <fengguang.wu@intel.com>
2018-08-31 10:28:55 +0800

    page-refs: skip nopage
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- page-refs.c
+++ page-refs.c
@@ -83,6 +83,10 @@ int count_refs(unsigned int max,
 		if (nrefs > max)
 			return 1;
 		if (g_kpageflags_buf) {
+			if (g_kpageflags_buf[pfn] & (1<<KPF_NOPAGE)) {
+				printdd("skip nopage %lx\n", pfn);
+				continue;
+			}
 			if (g_kpageflags_buf[pfn] & (1<<KPF_COMPOUND_TAIL)) {
 				printdd("skip tail %lx\n", pfn);
 				continue;
