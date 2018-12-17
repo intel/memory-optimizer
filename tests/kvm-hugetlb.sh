30040b285f35dd9e50675bdf9253b64cfbd0231b
yuanyao <yuan.yao@linux.intel.com>
2018-12-17 11:43:04 +0800

    Introduce kvm-hugetlb.sh for qemu run on hugetlbfs as memory backend
    we must set environment variable "interleave" to NUMA node set which
    already has enough hugetlbfs pages allocated, otherwise qemu can NOT
    boot up.
    
    For exmaple, my machine have 16 nodes(8 DRAM + 8 AEP):
    sudo sh -c "echo 65536 > nr_hugepages" will allocate 128G hugetlbfs
    pages dynamically and averagely on all 16 nodes, so we need set
    interleave to "0-15" if we want qemu to use all 128G memory here,
    of cause we can choose subset of all 16 nodes if qemu's memory
    setting is smaller.
    
    TO preallocate hugetlb pages at boot time, modify /etc/default/grub
    and add
    
    	default_hugepagesz=2M hugepagesz=2M hugepages=102400
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

---
+++ tests/kvm-hugetlb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+[[ $kernel ]] || kernel=$1
+[[ $kernel ]] || kernel=(
+	/c/linux/arch/x86/boot/bzImage
+)
+
+[[ $qemu_smp ]] || qemu_smp='cpus=32'
+[[ $qemu_mem ]] || qemu_mem='128G'
+[[ $qemu_ssh ]] || qemu_ssh='2222'
+[[ $qemu_log ]] && qemu_log=file:$qemu_log
+[[ $qemu_log ]] || qemu_log=stdio
+[[ $interleave ]] && numactl="numactl --interleave=$interleave"
+
+kvm=(
+	$numactl
+	qemu-system-x86_64
+	-machine pc,nvdimm
+	-cpu host
+	-enable-kvm
+	-kernel $kernel
+	-smp $qemu_smp
+	-numa node,nodeid=0,memdev=hgtlb0
+	-m $qemu_mem
+	-mem-prealloc
+	-object memory-backend-file,id=hgtlb0,size=$qemu_mem,mem-path=/dev/hugepages
+	-device virtio-net-pci,netdev=net0
+	-netdev user,id=net0,hostfwd=tcp::$qemu_ssh-:22
+	-no-reboot
+	-serial $qemu_log
+	-display none
+	-monitor null
+)
+
+append=(
+	ip=dhcp
+	nfsroot=10.0.2.2:/os/ubuntu,v3,tcp,intr,rsize=524288
+	root=/dev/nfs
+	ignore_loglevel
+	sysrq_always_enabled
+	panic=10
+	# prompt_ramdisk=0
+	earlyprintk=ttyS0,115200
+	console=ttyS0,115200
+	console=tty0
+	vga=normal
+	# root=/dev/ram0
+	rw
+)
+
+echo "kvm=${kvm[@]}"
+exec "${kvm[@]}" --append "${append[*]}"
