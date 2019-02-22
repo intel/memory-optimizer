25449fe5a4096881077c52753a2fdbe4de1b8eeb
Jin Yao <yao.jin@linux.intel.com>
2019-02-23 00:11:39 +0800

    hmd: Use GPL license header
    Signed-off-by: Jin Yao <yao.jin@linux.intel.com>

--- pmu-refs/cmsk.h
+++ pmu-refs/cmsk.h
@@ -1,18 +1,10 @@
 /*
- * Count-Min Sketch
+ * SPDX-License-Identifier: GPL-2.0
  *
- * Copyright (c) 2018 Intel Corporation
+ * Copyright (c) 2019 Intel Corporation
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that: (1) source code distributions
- * retain the above copyright notice and this paragraph in its entirety, (2)
- * distributions including binary code include the above copyright notice and
- * this paragraph in its entirety in the documentation or other materials
- * provided with the distribution
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Authors: Huang Ying <ying.huang@intel.com>
+ *          Jin Yao <yao.jin@intel.com>
  */
 
 #ifndef __CMSK__H__
