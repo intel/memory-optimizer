a02effab47ff0c72299419b16bdb6d3b3687da5c
yuanyao <yuan.yao@linux.intel.com>
2019-08-14 10:36:40 +0800

    Simplify the sysrefs_parser_hotness_drifting.rb once more
    Change to use different hash table for stable/unstable/percent values,
    use page type as key.

--- profile_tool/sysrefs_parser_hotness_drifting.rb
+++ profile_tool/sysrefs_parser_hotness_drifting.rb
@@ -1,25 +1,26 @@
 #!/bin/ruby
 
-$page = Hash.new {|hash, key| hash[key] = []}
+$stable = Hash.new {|hash, key| hash[key] = []}
+$unstable = Hash.new {|hash, key| hash[key] = []}
+$percent = Hash.new {|hash, key| hash[key] = []}
 
 while (line = STDIN.gets)
   case line
   when /(\w+)\s+:\s+stable\s+pages:(\d+)\s+unstable\s+pages:(\d+)\s+drifting\s+percent:(\d+)%/
-    $page[$1].push( {:stable => $2.to_i, :unstable => $3.to_i, :percent => $4.to_i} )
+    $stable[$1].push($2.to_i)
+    $unstable[$1].push($3.to_i)
+    $percent[$1].push($4.to_i)
   end
 end
 
-def calculate_hotness_drifting(result_array)
-  return 0, 0, 0 if result_array.empty?
+$percent.each_pair do |key, value_array|
+  max = value_array.max
+  min = value_array.min
+  if value_array.size != 0
+    avg = value_array.sum.to_f / value_array.size
+  else
+    avg = 0
+  end
 
-  max = result_array.max { |a, b| a[:percent] <=> b[:percent] }
-  min = result_array.min { |a, b| a[:percent] <=> b[:percent] }
-  total = result_array.sum { |a| a[:percent] }
-
-  return max[:percent], min[:percent], total.to_f / result_array.size
-end
-
-$page.each_pair do |key, value|
-  max, min, avg = calculate_hotness_drifting(value)
   print "%s: Max:%2d%%   Min:%2d%%   Avg:%2.2f%%\n" % [ key, max, min, avg ]
 end
