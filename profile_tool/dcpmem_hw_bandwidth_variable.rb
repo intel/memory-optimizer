1d6792ca3cb5447124ee159b75034426cc509030
yuanyao <yuan.yao@linux.intel.com>
2019-08-20 13:15:16 +0800

    Introduce dcpmem_hw_bandwidth_variable.ruby
    Include hw bandwidth variable definition, put into
    a solo file for easy customization.

---
+++ profile_tool/dcpmem_hw_bandwidth_variable.rb
@@ -0,0 +1,221 @@
+#!/usr/bin/env ruby
+$dcpmem_128g_hw_bandwidth = {
+  # 12w
+  "12" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+  },
+
+  # 15w
+  "15" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+  },
+
+  # 18w
+  "18" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+  },
+}
+
+
+$dcpmem_256g_hw_bandwidth = {
+  # 12w
+  "12" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+  },
+
+  # 15w
+  "15" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+
+  },
+
+  # 18w
+  "18" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+  },
+}
+
+
+$dcpmem_512g_hw_bandwidth = {
+  # 12w
+  "12" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+  },
+
+  # 15w
+  "15" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+
+  },
+
+  # 18w
+  "18" => {
+    "single" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "211" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+
+    },
+    "221" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+    "222" => {
+      "seq"  => {"read" => 0, "write" => 0},
+      "rand" => {"read" => 0, "write" => 0},
+    },
+  },
+}
+
+
+$dcpmem_hw_bandwidth = {
+  "128" => $dcpmem_128g_hw_bandwidth,
+  "256" => $dcpmem_256g_hw_bandwidth,
+  "512" => $dcpmem_512g_hw_bandwidth,
+}
